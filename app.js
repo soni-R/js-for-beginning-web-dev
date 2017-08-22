76e998870a0876aae6615e6f0db063289ffb8d2e
soni <soni.rukmaji@gmail.com>
2017-08-22 20:04:22 +0530

    About Console

--- app.js
+++ app.js
@@ -1,6 +1,39 @@
-// Popup boxes to display messages
-/* Prompt box */
-var name = prompt('Please enter your name');
+// console.log is used for debugging purposes
 
-alert('Hello '+ name);
+var name ='sam';
+console.log(name);
 
+var p = confirm('Are you sure');
+console.log(p);
+
+var number1 = 5,
+number2 = 10;
+result = number1 + number2;
+
+console.log(result);
+
+// we can manipulate variables, create variables in console.
+/* experiment in console
+ sam
+ app.js:5 true
+ 
+ number1 - number2;
+ -5
+
+ number1 + ' ' +number2
+"5 10"
+
+ number1 = 10
+ 10
+ number1 + number2
+ 20
+
+ name = "john"  //assigning a new value to a variable in console, which does not effect the main program, stays as is after refresh
+ "john"
+
+ name
+ "john"
+
+ name +' is '+ number2;
+ "john is 10"  */
+ 
