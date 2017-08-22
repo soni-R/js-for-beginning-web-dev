2d2d68c7aaf87d126b0e07b2e4d3096d33e8490f
soni <soni.rukmaji@gmail.com>
2017-08-22 23:57:47 +0530

    Logical Operators

--- app.js
+++ app.js
@@ -1,37 +1,43 @@
-var name='Mark';
-age =32;
-availableToday = false;
+var name='Mark',
+age = 30,
+ // availableToday = false;
 
-if(age === 32){
-	console.log('age is 32');
-}
+/*  Logical Operators
+	&& 	and 		
+	|| 	or 	
+	! 	not
+*/
 
-
-/*  Comparison Operators
-	'='    assigns value
-	'=='   checks something is equal to
-	'==='  strict equal, checks for value equality and type of the variable as well
-	'!='   not equal
-	'!=='  strict not equal, checks for not equal value or not equal type
-	'>'	   greater than 	 	
-	'<'	   less than 		
-	'>='   greater than or equal to  	
-	'<='   less than or equal to */
-
-// output in console
-// 	name
-// "Mark"
-// age
-// 32
-// age == 32
-// true
-// age === 32
-// true
-// age === '32' // === doesnot only check value it also checks type of the variable 
-// false		// since age is int type we passed 32 as a string so output is false
-// age == '32'
-// true
-// age != '32'
+//in console
+// !true
 // false
-// age !== '32' // !== operator also checks type of variable
+// !false
+// true
+// name == 'mark' && age == 30
+// false
+// name == 'Mark' && age == 30
+// true
+// name == 'Mark' && age == '30'
+// true
+// name === 'Mark' && age === '30';
+// false
+//  name === 'Mark' && age === 30;
+// true
+// name == 'Mark' || age === '30';
+// true
+// name == 'Mark' || age === '27';
+// true
+
+// (name === 'Mark' && age === 24) && availableToday;
+// false
+
+// name === 'Mark' && !availableToday
+// true
+
+ availableToday = true;
+
+//  name === 'Mark' && !availableToday
+// false
+
+// name === 'Mark' && availableToday
 // true
