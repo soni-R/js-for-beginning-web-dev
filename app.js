5b069164627cdc8aa22df930e883794c891a7e22
soni <soni.rukmaji@gmail.com>
2017-08-22 23:40:25 +0530

    About Concatination

--- app.js
+++ app.js
@@ -1,41 +1,8 @@
-var number1 = 10,
-number2 = 50,
-result; // typeof result is undefined since we hasn't had a value set to it
-// Note: undefined is different from not defined, undefined means no value is assigned, not defined means the specified variable is not exist in code.
-
-/* output in console
-typeof result
-"undefined"
-typeof number1
-"number" */
-
-if(typeof result === 'undefined') // === this not only checks the value for equal but also checks the type of a variable
-{
-	result = number1 + number2;
-}
-
-//NaN(not a number)
-var result2 = 'Alex';
-// result2/2
-// NaN
-// parseInt(result2)
-// NaN
-// output in console is NaN, for result2/2, since result2 is string type
-
-var result3 = '0Alex2';
-//parseInt(result3)
-//0    - gives what ever value available first
-
-//The isNaN() function determines whether a value is not a number or a number
-if(isNaN(result2 / 2)){
-	console.log("not a number");
-}
-
-num = 'a';
-result = num + number1;
-// result
-// "a10"
-
-result = parseInt(number1) + parseInt(num); // results NaN since num is string type.
-// result
-// NaN
+// concatinate different variables
+var name= 'Sam',
+age=30,
+availableToday = false;
+console.log('my name is ' + name);
+console.log(name + ' is my name');
+console.log('my name is ' + name + ' and my age is ' + age);
+console.log('my age is ' + age + ' in two years I will turn ' + (age + 2) + '. Available today? ' + availableToday);
