4305621211676578c9f259d3bc2d2ac1eff79667
soni <soni.rukmaji@gmail.com>
2017-08-24 18:04:12 +0530

    Countdown: Example Application

--- app.js
+++ app.js
@@ -1,55 +1,53 @@
-/* var getTime = function() {
-	console.log(new Date().getTime());
- },
-interval = setInterval(getTime, 1000);  //passing named function to setInterval
-*/
+var app = {};
 
+app.countdown = function(settings){
+	var interval,
+		counter = 0,
 
-// the below code too print the timestamp for every second like the above
-/*
-var interval = setInterval(function(){
-	console.log(new Date().getTime());
-}, 1000);
-*/
+		//start and end points
+		startAt = 0,
+		endAt = 0;
 
+	if(settings === undefined)
+	{
+		console.log('Please provide settings');
+	}	
+	else
+	{
+		if(settings.startAt === undefined || settings.endAt === undefined)
+		{
+			console.log('start and end numbers are required');
+		}
+		else
+		{
+			startAt = parseInt(settings.startAt, 10);
+			endAt = parseInt(settings.endAt, 10);
 
-// best way to display something for every specified timestamp
-var x = 1;
-interval = setInterval(function(){
-	console.log(x);
+			if(!isNaN(startAt) && !isNaN(endAt))
+			{
+				counter = startAt;
 
-	if(x === 5){
-		clearInterval(interval);
+				interval = setInterval(function(){
+
+					if(counter < endAt) 
+					{
+
+						counter = 0;
+						startAt = 0;
+						endAt = 0;
+
+						console.log('Finished');
+						clearInterval(interval);
+					}
+					else
+					{
+						console.log(counter);
+					}
+					
+					counter = counter - 1;
+					
+				}, 1000);
+			}
+		}
 	}
-	x++;
-}, 1000);
-
-
-//code that counts backwards
-/*var x = 5;
-interval = setInterval(function(){
-	console.log(x);
-
-	if(x === 1){
-		clearInterval(interval);
-	}
-	x--;
-}, 1000);
-*/
-
-
-// The below code prints hello for every 5 seconds, but it takes 5 secs to even print the initial output(hello) to the console, the reason is that we have not used a named function
-/*interval = setInterval(function(){
-	console.log('Hello');
-}, 5000);
-*/
-
-// code using named function which doesnot delay initial output
-/*var outputHello = function(){
-	console.log('Hello');
-},
-
-interval = setInterval(outputHello, 5000);
-
-outputHello();
-*/
+};
