9c178e895dbf3ac99ceede8fc521837091c194b5
soni <soni.rukmaji@gmail.com>
2017-08-24 14:11:12 +0530

    About Intervals

--- app.js
+++ app.js
@@ -1,15 +1,55 @@
-var date = new Date();
+/* var getTime = function() {
+	console.log(new Date().getTime());
+ },
+interval = setInterval(getTime, 1000);  //passing named function to setInterval
+*/
 
-/* in console
-date
-Thu Aug 24 2017 00:57:30 GMT+0530 (India Standard Time)
 
-date.getTime()
-1503516450894
+// the below code too print the timestamp for every second like the above
+/*
+var interval = setInterval(function(){
+	console.log(new Date().getTime());
+}, 1000);
+*/
 
-date.getFullYear()
-2017
 
-date.toJSON()
-"2017-08-23T19:27:30.894Z" 
+// best way to display something for every specified timestamp
+var x = 1;
+interval = setInterval(function(){
+	console.log(x);
+
+	if(x === 5){
+		clearInterval(interval);
+	}
+	x++;
+}, 1000);
+
+
+//code that counts backwards
+/*var x = 5;
+interval = setInterval(function(){
+	console.log(x);
+
+	if(x === 1){
+		clearInterval(interval);
+	}
+	x--;
+}, 1000);
+*/
+
+
+// The below code prints hello for every 5 seconds, but it takes 5 secs to even print the initial output(hello) to the console, the reason is that we have not used a named function
+/*interval = setInterval(function(){
+	console.log('Hello');
+}, 5000);
+*/
+
+// code using named function which doesnot delay initial output
+/*var outputHello = function(){
+	console.log('Hello');
+},
+
+interval = setInterval(outputHello, 5000);
+
+outputHello();
 */
