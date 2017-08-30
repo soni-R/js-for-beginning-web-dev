54f397b90e0a3339e46a0454a3da49fdbae495b7
soni <soni.rukmaji@gmail.com>
2017-08-31 01:47:01 +0530

    callback in countdown

--- app.js
+++ app.js
@@ -1,18 +1,58 @@
 var app = {};
- 
- app.add = function(numbers, callback){
- 	var result = 0;
 
- 	if(numbers !== undefined && numbers.length){
- 		for(number in numbers){
- 			//console.log(numbers[number]);
- 			result = result + numbers[number];
+app.countdown = function(settings, callback){
+	var interval,
+		counter = 0,
 
- 		}
- 		//console.log(result);
+		//start and end points
+		startAt = 0,
+		endAt = 0;
 
- 		if(callback !== undefined){
- 			callback(result);
- 		}
- 	}
- };
+	if(settings === undefined)
+	{
+		console.log('Please provide settings');
+	}	
+	else
+	{
+		if(settings.startAt === undefined || settings.endAt === undefined)
+		{
+			console.log('start and end numbers are required');
+		}
+		else
+		{
+			startAt = parseInt(settings.startAt, 10);
+			endAt = parseInt(settings.endAt, 10);
+
+			if(!isNaN(startAt) && !isNaN(endAt))
+			{
+				counter = startAt;
+
+				interval = setInterval(function(){
+
+					if(counter < endAt) 
+					{
+
+						counter = 0;
+						startAt = 0;
+						endAt = 0;
+
+						//console.log('Finished');
+
+						if(callback !== undefined){
+							callback();
+						} 
+						
+						clearInterval(interval);
+					}
+					else
+					{
+						console.log(counter);
+					}
+					
+					counter = counter - 1;
+					
+				}, 1000);
+			}
+		}
+	}
+};
